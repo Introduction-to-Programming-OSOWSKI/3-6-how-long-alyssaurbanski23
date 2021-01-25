0b62090d48ef4528efadea14f5dafb6676ae00de
alyssaurbanski23 <831554@mystma.org>
2021-01-25 18:04:50 +0000

    BP try 1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE IN THIS FILE
+def howL(x):
+
+    for i in range(0, len(x)):
+        if x[i] == "peanut butter":
+            return True
+
+        return False
+
+print (hasL("peanut butter"))
